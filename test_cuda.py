283bf74a319420f76f08ac68d5238deec54a98c3
Zoltan Kadar <pernix84@gmail.com>
2024-09-02 17:47:48 +0200

    THESIS-4: fix for flushing the stdout

--- test_cuda.py
+++ test_cuda.py
@@ -1,8 +1,11 @@
 from __future__ import print_function
-
 import torch
+import sys
+
 x = torch.rand(5, 3)
 print(x)
 
 print("=============>")
 print("Is cuda enabled? {0}".format(torch.cuda.is_available()))
+
+sys.stdout.flush()
